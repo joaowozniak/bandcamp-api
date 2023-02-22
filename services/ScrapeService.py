f6af5d4e4de8d73f95883a63e971ee6a51080fd1
joaowozniak <joao.wozniak@gmail.com>
2023-02-22 19:56:01 +0000

    refactor scrape service

--- services/ScrapeService.py
+++ services/ScrapeService.py
@@ -1,26 +1,38 @@
 from bs4 import BeautifulSoup
-from utils.utils import Utils
 import json
+from typing import List
+
+from utils.utils import Utils
 
 
 class ScrapeService:
-
     @staticmethod
-    def scrape_page(url: str):
-        html_text = Utils.execute_get_request(url).text
-        soup = BeautifulSoup(html_text, "lxml")
+    def scrape_page(url: str) -> dict:
+        """
+        Scrape the Bandcamp page at the given URL and return the data-blob as a dictionary.
+        """
+        html = Utils.execute_get_request(url).text
+        soup = BeautifulSoup(html, "lxml")
         if soup:
-            return json.loads(soup.find(id="pagedata")["data-blob"])
+            pagedata = soup.find(id="pagedata")
+            if pagedata:
+                return json.loads(pagedata["data-blob"])
+        return {}
 
     @staticmethod
-    def scrape_aotd_page(url: str):
-        page_html = Utils.execute_get_request(url).text
-        soup = BeautifulSoup(page_html, "lxml")
+    def scrape_aotd_page(url: str) -> List[List[str]]:
+        """
+        Scrape the Bandcamp AOTD page at the given URL and return a list of lists containing the artist name and album title.
+        """
+        html = Utils.execute_get_request(url).text
+        soup = BeautifulSoup(html, "lxml")
         results = []
         if soup:
             for div in soup.find_all("div", {"class": "list-article aotd"}):
-                for a, b in zip(div.find_all("div", {"class": "article-info-text"}),
-                                div.find_all("div", {"class": "title-wrapper"})):
-                    results.append(
-                        [a.contents[4].strip(), b.contents[0].contents[0].strip().replace('”', '').replace('“', '')])
+                article_info = div.find("div", {"class": "article-info-text"})
+                title_wrapper = div.find("div", {"class": "title-wrapper"})
+                if article_info and title_wrapper:
+                    artist = article_info.contents[4].strip()
+                    album = title_wrapper.contents[0].contents[0].strip().replace('”', '').replace('“', '')
+                    results.append([artist, album])
         return results
